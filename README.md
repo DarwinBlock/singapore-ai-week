1eb23558dd65accd0538ee00c763e5cae7e8c373
pushpit07 <pushpit07@gmail.com>
2024-02-10 14:26:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Token2049 Week Events Listing App
+
+Welcome to the Token2049 Week Events Listing App! This web application provides a user-friendly interface to explore and view all events scheduled for Token2049 Week, taking place from 15th to 21st April 2024.
+
+## Technologies Used
+
+- **NextJS v14:**
+  - The UI is built using NextJS v14.
+  
+- **API Endpoints:**
+  - API endpoints are integrated directly within the UI using NextJS API routes.
+
+- **CSS Frameworks:**
+  - Tailwind CSS and Bootstrap CSS have been utilized for styling, ensuring appealing and responsive design.
+
+- **Database:**
+  - The database (mysql-token-week) is provisioned on DigitalOcean.
+
+- **Deployment:**
+  - The web app is deployed using the App (serverless) service offered by DigitalOcean.
---
+++ components/NewsLetter.jsx
@@ -0,0 +1,25 @@
+const NewsLetter = () => {
+    return (  
+        <div className="alldata subs_section">
+            <div className="subs row">
+                <div className="container">
+                    <div className="col-md-9 col-sm-12 subs-mid">
+                        <div className="subs-heads">
+                            <h2 className="subs-txt">Subscribe for the latest event updates</h2>
+                        </div>
+                        <div className="subs-inpt d-flex">
+                            <input type="text" name="" className="email" placeholder="Email Address" />
+                            <a href="https://token2049.us17.list-manage.com/subscribe/post?u=0a78eaa9e8ac8f5ee04b715d3&id=5460c9b150">
+                                <button>
+                                GET UPDATES
+                                </button>
+                                </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default NewsLetter;
