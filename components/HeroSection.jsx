4985e545296b62536c82595b8e2356f81c6065b5
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2024-02-11 18:07:36 +0530

    init setup

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,13 +1,13 @@
 const HeroSection = () => {
     return (  
         <div className="ban-ner">
-            <div className="header-fix-bg"></div>
+            {/* <div className="header-fix-bg"></div> */}
             <div className="inner-img">
                 <div className="container">
                     <div className="inner-text">
                         <img src={'/TOKEN2049-Week-Logo.png'} className="desktop" alt="Banner"/>
                         <img src={'/TOKEN2049-Week-Logo-Stacked.png'} className="mobile" />
-                        <p>15 - 21 APRIL 2024 <span className="dxtop">| DUBAI</span> </p>
+                        <p>03 - 09 June 2024 &middot; <span className="dxtop">Mariana bay sands, SINGAPORE</span> </p>
                         <div className="own-event">
 
                             <p className="Drinks">
@@ -21,12 +21,12 @@ const HeroSection = () => {
                             by using <b>#TOKEN2049Week</b> on Twitter.
                             </p>
                             <div className="own-evnt-btn d-flex">
-                            <a href="https://forms.token2049.com/token2049-week-event-submission" target="_blank">
-                                <button className="side-evnt">SUBMIT YOUR EVENT</button>
-                            </a>
                             <a className="cursor-pointer" href="https://docs.google.com/spreadsheets/d/1_mUbQe7AjxHMdXzb2rySYgb5YPA2-o8dZN1nAHCICto/edit#gid=0"
                                 target="__blank">
-                                <button className="hst-own cursor-pointer">SIDE EVENTS SHEET</button>
+                                <button className="min-w-[239px] hst-own cursor-pointer">SIDE EVENTS SHEET</button>
+                            </a>
+                            <a href="https://forms.token2049.com/token2049-week-event-submission" target="_blank">
+                                <button className="min-w-[239px] side-evnt">HOST YOUR OWN EVENT</button>
                             </a>
                             </div>
                         </div>
